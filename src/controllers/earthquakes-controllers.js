34a9be14a292b115ae656056d82014076fbecb5e
LeoQuark <leonardo.penaf@utem.cl>
2021-07-09 00:49:40 -0400

    reemplazo de puppeteer a cheerio por tiempo de ejecucion

--- src/controllers/earthquakes-controllers.js
+++ src/controllers/earthquakes-controllers.js
@@ -1,74 +1,76 @@
-import puppeteer from "puppeteer";
+import cheerio from "cheerio";
+import request from "request-promise";
 import Pool from "../database/connection";
 
 export const getSismos = async (req, res) => {
   try {
-    //Inicio de puppeteer
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-    await page.setViewport({ width: 1920, height: 1080 });
-    await page.goto("http://www.sismologia.cl/links/ultimos_sismos.html");
-
-    const infoSismos = await page.evaluate(() => {
-      //Obtengo el tbody de la tabla principal de sismos chile
-      const tbody = document.querySelectorAll("table tbody");
-      const sismos = [];
-
-      //Recorro todos los nodos hijos (tr) del tbody (15 en total)
-      for (let tr of tbody) {
-        //Recorro los nodos hijos de cada tr, es decir, obtengo todos los td
-        for (let i = 1; i < tr.childElementCount; i++) {
-          //Declaro un objeto con los atributos necesarios
-          const info = {
-            fecha_local: "",
-            fecha_utc: "",
-            latitud: "",
-            longitud: "",
-            profundidad: "",
-            magnitud: "",
-            agencia: "",
-            referencia_geografica: "",
-          };
-
-          //Asignación de los datos obtenidos al objeto
-          info.fecha_local = tr.children[i].children[0].textContent;
-          info.fecha_utc = tr.children[i].children[1].textContent;
-          info.latitud = tr.children[i].children[2].textContent;
-          info.longitud = tr.children[i].children[3].textContent;
-          info.profundidad = tr.children[i].children[4].textContent;
-          info.magnitud = tr.children[i].children[5].textContent;
-          info.agencia = tr.children[i].children[6].textContent;
-          info.referencia_geografica = tr.children[i].children[7].textContent;
-
-          //Agrego cada objeto a un arreglo
-          sismos.push(info);
-        }
-      }
-      return sismos;
+    const objeto = [];
+    const $ = await request({
+      //uri: 'http://www.sismologia.cl/events/sensibles/2021/07/08-2342-55L.S202107.html',
+      uri: "http://www.sismologia.cl/links/ultimos_sismos.html",
+      transform: (body) => cheerio.load(body),
     });
-    await browser.close();
+
+    $("tbody tr")
+      .next()
+      .each((i, el) => {
+        const sismos = [];
+
+        let aux = {
+          fecha_local: "",
+          fecha_utc: "",
+          latitud: "",
+          longitud: "",
+          profundidad: "",
+          magnitud: "",
+          agencia: "",
+          referencia_geografica: "",
+        };
+        $(el)
+          .find("td")
+          .each((j, il) => {
+            if (j == 0) {
+              const text = $(el).find("a");
+              sismos.push(text.html());
+              //console.log(j,text.html());
+            } else {
+              sismos.push($(il).html());
+
+              //console.log(j, $(il).html());
+            }
+          });
+
+        aux.fecha_local = sismos[0];
+        aux.fecha_utc = sismos[1];
+        aux.latitud = sismos[2];
+        aux.longitud = sismos[3];
+        aux.profundidad = sismos[4];
+        aux.magnitud = sismos[5];
+        aux.agencia = sismos[6];
+        aux.referencia_geografica = sismos[7];
+
+        objeto.push(aux);
+      });
 
     //Consulta de prueba, agrego el ultimo temblor registrado
-    const consulta = await Pool.query(
-      "INSERT INTO sismos (fecha_local,fecha_utc,latitud,longitud,profundidad,magnitud,agencia,referencia_geografica) VALUES ($1,$2,$3,$4,$5,$6,$7,$8)",
-      [
-        infoSismos[0].fecha_local,
-        infoSismos[0].fecha_utc,
-        infoSismos[0].latitud,
-        infoSismos[0].longitud,
-        infoSismos[0].profundidad,
-        infoSismos[0].magnitud,
-        infoSismos[0].agencia,
-        infoSismos[0].referencia_geografica,
-      ]
-    );
+    // const consulta = await Pool.query(
+    //   "INSERT INTO sismos (fecha_local,fecha_utc,latitud,longitud,profundidad,magnitud,agencia,referencia_geografica) VALUES ($1,$2,$3,$4,$5,$6,$7,$8)",
+    //   [
+    //     infoSismos[0].fecha_local,
+    //     infoSismos[0].fecha_utc,
+    //     infoSismos[0].latitud,
+    //     infoSismos[0].longitud,
+    //     infoSismos[0].profundidad,
+    //     infoSismos[0].magnitud,
+    //     infoSismos[0].agencia,
+    //     infoSismos[0].referencia_geografica,
+    //   ]
+    // );
 
-    if (consulta) {
-      res.status(200).json({
-        msg: "API funcionando",
-        data: infoSismos[0],
-      });
-    }
+    res.status(200).json({
+      msg: "API funcionando",
+      data: objeto,
+    });
   } catch (error) {
     console.log(error);
     res.json({
