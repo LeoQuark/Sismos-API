a3cc001ef807bd61d39625518f0cdcca76a9026c
LeoQuark <leonardo.penaf@utem.cl>
2021-07-22 22:22:00 -0400

    obtener sismo por id_sismo

--- src/controllers/earthquakes-controllers.js
+++ src/controllers/earthquakes-controllers.js
@@ -4,9 +4,7 @@ import zonaLocal from "../helpers/fecha";
 //Peticion para obtener los 15 primeros datos de la bd
 export const getSismos = async (req, res) => {
   try {
-    const consulta = await Pool.query(
-      "SELECT fecha_local,latitud,longitud,profundidad,magnitud,referencia_geografica FROM sismos LIMIT 15"
-    );
+    const consulta = await Pool.query("SELECT * FROM sismos LIMIT 15");
 
     if (consulta.rowCount != 0) {
       res.status(200).json({
@@ -22,6 +20,32 @@ export const getSismos = async (req, res) => {
       tipoMIME: "application/json",
       fecha: zonaLocal(),
       mensaje: error,
+    });
+  }
+};
+
+export const getSismoById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const consulta = await Pool.query(
+      "SELECT * FROM sismos WHERE id_sismo=$1",
+      [id]
+    );
+
+    const sismo = consulta.rows[0];
+
+    if (sismo) {
+      res.status(200).json({
+        status: "200",
+        tipoMIME: "application/json",
+        sismo,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      msg: "Error",
+      error,
     });
   }
 };
--- src/routes/earthquakes.js
+++ src/routes/earthquakes.js
@@ -4,12 +4,17 @@ import { Router } from "express";
 import { validateJwt } from "../middlewares/validateJWT";
 import { validarURL } from "../middlewares/validation";
 //Controllers
-import { getSismos } from "../controllers/earthquakes-controllers";
+import {
+  getSismos,
+  getSismoById,
+} from "../controllers/earthquakes-controllers";
 //Rutas
 const router = Router();
 
 //GET
 //Valida si existe un token valido en el header de la peticion
-router.get("/earthquakes", validateJwt, getSismos);
+router.get("/earthquakes", getSismos);
+
+router.get("/earthquakes/:id", getSismoById);
 
 export default router;
